b19fb415b3412d65067522926d8df52cb3fc20d8
Kosmik <malpa.w.klapkach@gmail.com>
2020-10-22 02:36:18 +0200

    Added PlaySound class

---
+++ Assets/Scripts/Actions/PlaySound.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlaySound : Action
+{
+    public new AudioClip audio;
+    public bool waitUntilEnd;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = StateOfGame.main.GetComponent<AudioSource>();
+    }
+    public override void DoAction()
+    {
+        if (waitUntilEnd)
+            isActing = true;
+
+        AudioSource audioSource = StateOfGame.main.GetComponent<AudioSource>();
+        audioSource.clip = audio;
+        audioSource.Play();
+    }
+
+    private void Update()
+    {
+        if (isActing && !audioSource.isPlaying)
+            isActing = false;
+    }
+}
+
+
+
+
